0ca573088a093bca77598d5fa9892f8c05bda11b
Omkar Khilari <omkhilariindia@gmail.com>
2022-01-20 09:43:18 +0530

    only strings

--- main.py
+++ main.py
@@ -44,7 +44,7 @@ if uploaded_file is not None:
 
     op2='\n'
     for col in xl.columns:
-        op2 += col + ' INT,\n' if str(xl[col][0]).isdigit() else col + ' VARCHAR(' + str(len(max(xl[col]))) + '),' + '\n'
+        op2 += col + ' VARCHAR(' + str(len(max(xl[col]))) + '),' + '\n'
 
 
     output = st.text_area('Create Table Code : '
